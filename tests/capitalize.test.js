1bcb0a46e74de3a09b410a954309ed927481c1b1
mpwow5 <mpwow5@gmail.com>
2023-05-10 20:46:07 +0300

    Second

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,5 @@
 import { capitalize } from "../src/capitalize.js";
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Error!');
-}
 
-if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
-
-console.log('Success!')
+asserts.deepEqual(capitalize('hello') === 'Hello');
+asserts.deepEqual(capitalize('') === '');
